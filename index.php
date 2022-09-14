da3412eb10fb3a8d04744f62bdb15b9770a16f91
bintaraaja <113499546+bintaraaja@users.noreply.github.com>
2022-09-14 14:38:26 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,38 @@
 <!DOCTYPE html>
+<html>
+<head>
+  <title>Login Form</title.
+  <link rel="stylesheet" href="style.css" type="text/css">
+ </heat>
+ <body>
+   
+   <!--<h2>Login From </h2>  -->
+   
+   <form action="tampil.php"  method="POST">
+     <h2>Form Login </h2>
+     <div class="imgcontaine">
+          <img src="icon.png" alt="Avatar" class="avatar">
+     </div>
+     
+     <div class="container">
+       <label><b>username</b></label> 
+       <input type="text" placeholder="Masukkan username
+ name="username"    required>
+                
+                <label><b>password</b></label>
+                <input type="password" placeholder="Masukkan password
+    name="email" required>
+       
+       <button type="submit">Masuk</button>
+       <input type="checbox" checked="checked"><span>ingat saya</span>
+     </div>
+     <div class="container' style="background-color:#f1f1f1">
+             <button type="button" class="cancelbtn">Batal</button>
+             <span class="psw">Lupa <a href="#">password?</a></span>
+          </div>       
+          </form>       
+                                 
+           </body>
+                 </html>
+                                               
+                                                                                      
