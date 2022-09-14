b12d608e9365f8f012c0ab9588327f7e76ba6119
bintaraaja <113499546+bintaraaja@users.noreply.github.com>
2022-09-14 15:38:08 +0700

    Update tampil.php

--- tampil.php
+++ tampil.php
@@ -1,1 +1,15 @@
 <!DOCTYPE html>
+<html>
+<head>
+  <title>Hasil Login</title>
+  <link rel="stylesheet" href="style.css" type="text/css">
+  </head>             
+  <body>      
+        
+  <?php
+   $nama = $_POST["username"];
+    $email = $_POST["email"];
+    
+    ?>
+    <form action="tampil.php" methot="post;>
+     
